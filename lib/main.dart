4375a4d9cf301ee295e59d0d5ba5d20246e4baad
Eleaus-Hossain-Evan <eleaus16-364@diu.edu.bdm>
2023-06-14 03:20:21 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:atomsbox/atomsbox.dart';
+import 'package:flutter/material.dart';
+
+import 'ui/home/views/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: AppTheme.darkTheme,
+      home: const HomeScreen(),
+    );
+  }
+}
